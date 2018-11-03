a1a42134144ffaac9607d9ec4b2ab6d4ddf7a8da
Wilder Garcia <wgarcia2@uw.edu>
2018-11-03 13:06:59 -0700

    create fragment

---
+++ app/src/main/java/group6/tcss450/uw/edu/chatapp/utils/Weather_Fragment.java
@@ -0,0 +1,30 @@
+package group6.tcss450.uw.edu.chatapp.utils;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import group6.tcss450.uw.edu.chatapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Weather_Fragment extends Fragment {
+
+
+    public Weather_Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_weather_, container, false);
+    }
+
+}
