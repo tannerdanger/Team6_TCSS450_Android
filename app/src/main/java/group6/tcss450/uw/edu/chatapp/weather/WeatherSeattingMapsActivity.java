a6257dda82069dcb46d69c83ead2694e86cd5c2c
wilderG <wemanuel22@hotmail.com>
2018-11-30 07:06:13 -0800

    Revert "update version (#69)"
    This reverts commit f395d86bdb37f024eadf2af67d04c9158a1155cc.

--- app/src/main/java/group6/tcss450/uw/edu/chatapp/weather/WeatherSeattingMapsActivity.java
+++ app/src/main/java/group6/tcss450/uw/edu/chatapp/weather/WeatherSeattingMapsActivity.java
@@ -1,19 +1,13 @@
 package group6.tcss450.uw.edu.chatapp.weather;
 
-<<<<<<< HEAD
-=======
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
->>>>>>> 30fd5e3a4146fc36f518853d534d3fb37bef1b5a
 import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
-<<<<<<< HEAD
-=======
 import android.widget.Toast;
->>>>>>> 30fd5e3a4146fc36f518853d534d3fb37bef1b5a
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -25,29 +19,20 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import group6.tcss450.uw.edu.chatapp.R;
 
-<<<<<<< HEAD
-=======
 import static android.location.LocationManager.GPS_PROVIDER;
 
->>>>>>> 30fd5e3a4146fc36f518853d534d3fb37bef1b5a
 public class WeatherSeattingMapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
 
     private GoogleMap mMap;
     private Location mCurrentLocation;
-<<<<<<< HEAD
-=======
     private double mLat;
     private double mLon;
->>>>>>> 30fd5e3a4146fc36f518853d534d3fb37bef1b5a
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather_seatting_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-<<<<<<< HEAD
-        mCurrentLocation = (Location) getIntent().getParcelableExtra("LOCATION");
-=======
         mCurrentLocation = new Location(GPS_PROVIDER);
 
         mLat = getIntent().getDoubleExtra("lat", -1 );
@@ -58,15 +43,11 @@ public class WeatherSeattingMapsActivity extends FragmentActivity implements OnM
             mCurrentLocation.setLongitude(mLon);
         }
 
->>>>>>> 30fd5e3a4146fc36f518853d534d3fb37bef1b5a
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-<<<<<<< HEAD
-=======
 
->>>>>>> 30fd5e3a4146fc36f518853d534d3fb37bef1b5a
     }
 
     /**
@@ -84,12 +65,9 @@ public class WeatherSeattingMapsActivity extends FragmentActivity implements OnM
 
         // Add a marker in the current device location and move the camera
         LatLng current = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
-<<<<<<< HEAD
-=======
 
 
 
->>>>>>> 30fd5e3a4146fc36f518853d534d3fb37bef1b5a
         mMap.addMarker(new MarkerOptions().position(current).title("Current Location"));
         //Zoom levels are from 2.0f (zoomed out) to 21.f (zoomed in)
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(current, 15.0f));
@@ -99,14 +77,6 @@ public class WeatherSeattingMapsActivity extends FragmentActivity implements OnM
 
     @Override
     public void onMapClick(LatLng latLng) {
-<<<<<<< HEAD
-        Log.d("LAT/LONG", latLng.toString());
-        Marker marker = mMap.addMarker(new MarkerOptions()
-                .position(latLng)
-                .title("New Marker"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 18.0f));
-    }
-=======
 
         mMap.clear(); //remove old marker
 
@@ -136,7 +106,6 @@ public class WeatherSeattingMapsActivity extends FragmentActivity implements OnM
 
 
 
->>>>>>> 30fd5e3a4146fc36f518853d534d3fb37bef1b5a
 }
 
 
