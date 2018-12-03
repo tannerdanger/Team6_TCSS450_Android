263427a82872585d78d966e9139c7af63227e159
Eli Shafer <eli.shafer96@gmail.com>
2018-12-03 14:23:35 -0800

    Friend request notification fixed.

--- app/src/main/java/group6/tcss450/uw/edu/chatapp/view/MyNotificationRecyclerViewAdapter.java
+++ app/src/main/java/group6/tcss450/uw/edu/chatapp/view/MyNotificationRecyclerViewAdapter.java
@@ -61,7 +61,7 @@ public class MyNotificationRecyclerViewAdapter extends RecyclerView.Adapter<MyNo
                 if (null != mListener) {
                     // Notify the active callbacks interface (the activity, if the
                     // fragment is attached to one) that an item has been selected.
-//                    mListener.onListFragmentInteraction(holder.mItem);
+                    mListener.onListNotificationFragmentInteraction(holder.mItem);
                 }
             }
         });
